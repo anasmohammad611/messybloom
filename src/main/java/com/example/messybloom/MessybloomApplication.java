643428630ede206833224b649369250773d0968d
anas <anas@healthi.in>
2023-11-09 19:46:49 +0530

    Initialised messybloom

---
+++ src/main/java/com/example/messybloom/MessybloomApplication.java
@@ -0,0 +1,13 @@
+package com.example.messybloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessybloomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessybloomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/messybloom/MessybloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messybloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessybloomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
